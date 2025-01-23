d1f6de836a7a1ad2aaf3323a811c6e4227606512
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2025-01-23 12:55:29 -0500

    Add project files.

---
+++ BasicMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capirchio_Mathematics
+{
+    public class BasicMath
+    {
+        public double AddNumbers(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double SubtractNumbers(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double MultiplyNumbers(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double DivideNumbers(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
